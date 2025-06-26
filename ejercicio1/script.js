a2fb240afb7b71284c2358936e58d4194be7b746
Torres Marcos Emanuel <jinmoreh@gmail.com>
2025-06-26 18:36:33 -0300

    agregue la funsion desactivar si divido

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -3,3 +3,11 @@ const numero2 = document.getElementById('numero2');
 const operacion = document.getElementById('operacion');
 const calcularBtn = document.getElementById('calcular');
 const resultado = document.getElementById('resultado');
+
+operacion.addEventListener('change', function () {
+  if (operacion.value === 'dividir') {
+    calcularBtn.disabled = true;
+  } else {
+    calcularBtn.disabled = false;
+  }
+});
