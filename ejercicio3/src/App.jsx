81b4010a1db87453c35f597ed12c81b380d0cb69
Torres Marcos Emanuel <jinmoreh@gmail.com>
2025-06-26 20:22:43 -0300

    agregue la llamada a la api

--- ejercicio3/src/App.jsx
+++ ejercicio3/src/App.jsx
@@ -4,28 +4,16 @@ function App() {
   const [tareas, setTareas] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(res => res.json())
-      .then(data => {
-        const tareasCompletadas = data
-          .filter(tarea => tarea.completed)
-          .map(tarea => ({
-            ...tarea,
-            title: `✅ Tarea completada #${tarea.id} del usuario ${tarea.userId}`
-          }));
-        setTareas(tareasCompletadas);
-      })
-      .catch(error => console.error('Error al obtener tareas:', error));
-  }, []);
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(data => {})
+    .catch(error => console.error('Error al obtener tareas:', error));
+}, []);
 
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial' }}>
       <h1>Tareas Completadas</h1>
-      <ul>
-        {tareas.map(tarea => (
-          <li key={tarea.id}>{tarea.title}</li>
-        ))}
-      </ul>
+      <ul></ul>
     </div>
   );
 }
