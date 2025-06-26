1ecb484965f4d7268cee3b8aee9a210c4139994c
Torres Marcos Emanuel <jinmoreh@gmail.com>
2025-06-26 18:32:45 -0300

    agregue los elementos para el DOM

---
+++ ejercicio1/script.js
@@ -0,0 +1,5 @@
+const numero1 = document.getElementById('numero1');
+const numero2 = document.getElementById('numero2');
+const operacion = document.getElementById('operacion');
+const calcularBtn = document.getElementById('calcular');
+const resultado = document.getElementById('resultado');
