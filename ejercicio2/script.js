7ddd7b07db18b6189b099211f376a4aa7a415e23
Torres Marcos Emanuel <jinmoreh@gmail.com>
2025-06-26 18:44:30 -0300

    agregue la funsion mostrar lista

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -3,3 +3,12 @@ const input = document.getElementById("busqueda");
 const btnFiltrar = document.getElementById("filtrar");
 const ul = document.getElementById("listaPalabras");
 const mensaje = document.getElementById("mensaje");
+
+function mostrarLista(lista) {
+  ul.innerHTML = "";
+  lista.forEach(palabra => {
+    const li = document.createElement("li");
+    li.textContent = palabra;
+    ul.appendChild(li);
+  });
+}
