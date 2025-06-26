6ffdd93308683cc428fb8211b4f8540d3229c286
Torres Marcos Emanuel <jinmoreh@gmail.com>
2025-06-26 18:44:04 -0300

    agregue los elementos para el DOM

---
+++ ejercicio2/script.js
@@ -0,0 +1,5 @@
+const palabras = ["manzana", "banana", "pera", "durazno", "frutilla", "mango"];
+const input = document.getElementById("busqueda");
+const btnFiltrar = document.getElementById("filtrar");
+const ul = document.getElementById("listaPalabras");
+const mensaje = document.getElementById("mensaje");
